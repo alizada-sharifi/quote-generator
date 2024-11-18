e26cb7cee0994c19d06bb8a6fd5c246f7ef2f542
A.Sharifi <alizadasharifi2022@gmail.com>
2024-11-17 18:23:25 -0600

    first commit

---
+++ script.js
@@ -0,0 +1,39 @@
+const quoteContainer = document.getElementById("quote-container");
+const quoteText = document.getElementById("quote");
+const author = document.getElementById("author");
+const twitterBtn = document.getElementById("twitter");
+const newQuoteBtn = document.getElementById("new-quote");
+const loader = document.getElementById("loader");
+// ============
+function newQuote() {
+  loading();
+  const quote = localQuotes[Math.floor(Math.random() * localQuotes.length)];
+  if (!quote.author) {
+    author.textContent = "Unknown";
+  } else {
+    author.textContent = quote.author;
+  }
+  if (quote.text.length > 120) {
+    author.classList.add("long-quote");
+  } else {
+    author.classList.remove("long-quote");
+  }
+  quoteText.textContent = quote.text;
+  complete();
+}
+function loading() {
+  loader.hidden = false;
+  quoteContainer.hidden = true;
+}
+function complete() {
+  loader.hidden = true;
+  quoteContainer.hidden = false;
+}
+function twittThis() {
+  const twitterUrl = `https://twitter.com/intent/tweet?text=${quoteText.textContent} - ${author.textContent}`;
+  window.open(twitterUrl, "_blank");
+}
+twitterBtn.addEventListener("click", twittThis);
+newQuoteBtn.addEventListener("click", newQuote);
+// ===== on load ======
+newQuote();
